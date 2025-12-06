505f027a3016aca7b1df4713b07623f4757c1d05
mtask <13092724+mtask@users.noreply.github.com>
2025-12-07 00:28:55 +0200

    collect module

--- triage.py
+++ triage.py
@@ -2,8 +2,15 @@ import yaml
 import argparse
 import json
 import os
+import sys
+import logging
 from jinja2 import Environment, FileSystemLoader
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
 
 def load_config(path):
     with open(path, "r", encoding="utf-8") as f:
@@ -28,16 +35,26 @@ def main(args):
     pattern_result = []
     yara_result = []
     if args.pattern:
+        logging.info("Running pattern module")
         import modules.mod_pattern as mp
         mod_pattern = config['modules']['pattern']
         pattern_result = mp.search(mod_pattern['patterns_dir'], args.target_path)
         print(json.dumps(pattern_result, indent=2))
     if args.yara:
+        logging.info("Running yara module")
         import modules.mod_yara as my
         mod_yara = config['modules']['yara']
         yara_result = my.search(mod_yara['rules_dir'], args.target_path)
         print(json.dumps(yara_result, indent=2))
-    report(yara_data=yara_result, pattern_data=pattern_result)
+    if args.collect:
+        logging.info("Running collect module")
+        import modules.mod_collect as mc
+        mod_collect = config['modules']['collect']
+        #mc.commands(mod_collect['outdir'], mod_collect['commands'])
+        #mc.find_luks_devices(mod_collect['outdir'])
+        mc.checksums(mod_collect['outdir'], mod_collect['checksums'])
+        #mc.files_and_dirs(mod_collect['outdir'], mod_collect['files_and_dirs'])
+    #report(yara_data=yara_result, pattern_data=pattern_result)
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -65,6 +82,12 @@ def parse_args():
         "--pattern",
         action='store_true',
         help="Enable pattern module"
+    )
+
+    parser.add_argument(
+        "--collect",
+        action='store_true',
+        help="Enable collect module"
     )
 
     args = parser.parse_args()
